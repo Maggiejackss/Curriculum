1c0a4dafea61c16f185a8a879423879611ffedc4
Maggie <zach.benton.27@gmail.com>
2023-01-26 21:20:52 -0500

    add new folders and more classwork

---
+++ digital-crafts/javascript/exercises/jan26.js
@@ -0,0 +1,66 @@
+const months = [
+    {   monthName: 'January',
+        monthAbrv: 'Jan',
+        monthNum: 1,
+        monthDC: 32
+    },
+    {   monthName: 'February',
+        monthAbrv: 'Feb',
+        monthNum: 2,
+        monthDC: 28
+    },
+    {   monthName: 'March',
+        monthAbrv: 'Mar',
+        monthNum: 31,
+        monthDC: 30
+    },
+    {   monthName: 'April',
+        monthAbrv: 'Apr',
+        monthNum: 4,
+        monthDC: 30
+    },
+    {   monthName: 'May',
+        monthAbrv: 'May',
+        monthNum: 5,
+        monthDC: 31
+    },
+    {   monthName: 'June',
+        monthAbrv: 'Jun',
+        monthNum: 6,
+        monthDC: 30
+    },
+    {   monthName: 'July',
+        monthAbrv: 'Jul',
+        monthNum: 7,
+        monthDC: 31
+    },
+    {   monthName: 'August',
+        monthAbrv: 'Aug',
+        monthNum: 8,
+        monthDC: 31
+    },
+    {   monthName: 'September',
+        monthAbrv: 'Sep',
+        monthNum: 9,
+        monthDC: 30
+    },
+    {   monthName: 'October',
+        monthAbrv: 'Oct',
+        monthNum: 10,
+        monthDC: 31
+    },
+    {   monthName: 'November',
+        monthAbrv: 'Nov',
+        monthNum: 11,
+        monthDC: 30
+    },
+    {   monthName: 'December',
+        monthAbrv: 'Dec',
+        monthNum: 12,
+        monthDC: 31
+    }
+];
+
+console.log(months[8].monthDC);
+console.log(months[1].monthAbrv);
+console.log(months[3].monthName);
---
+++ homework/week1.md
@@ -0,0 +1,14 @@
+# Homework
+
+## Week 1
+|Assignment|Grade|
+|Terminus|9|
+|CLI|9|
+|Shell|9|
+Git
+CLI
+Shell
+Shell
+JS
+Alias
+Alias
