295ba8021357d0253fc1ef5841b5c1248d4c2916
Maggie <zach.benton.27@gmail.com>
2023-02-11 10:44:12 -0500

    add and modify 90s website files

---
+++ html/90s Website/jsMain.js
@@ -0,0 +1,11 @@
+document.getElementById("Home").addEventListener('click', clickHome);
+
+function clickHome() {
+    location.replace('./indexMain.html');
+}
+
+document.getElementById("Beginning").addEventListener('click', clickBeg);
+
+function clickBeg() {
+    location.replace('./indexBeg.html');
+}
